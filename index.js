d83ea965cf6c6f759db14d265c64062586e60596
Elyse Burmester <burmesterelyse@gmail.com>
2019-05-26 18:53:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 var customerName = "bob";
 const leastFavoriteCustomer = undefined;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+  return bestCustomer;
+}
+
+function overwriteBestCustomer(customer) {
+  bestCustomer = customer;
+  return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "customer";
+}  
